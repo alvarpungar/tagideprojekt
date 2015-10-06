faa6ef585f0fd306da99fd2d0dee1f122bf9912e
alvarpungar <alvar.pungar@khk.ee>
2015-10-06 20:40:13 +0300

    Algus ja lõpp

---
+++ main.php
@@ -0,0 +1,235 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <base href="www.khk.ee">
+    <meta name="Minuoma">
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="stylesheet.css">
+    <title>tagideprojekt</title>
+</head>
+<body>
+<header>
+    <p id="header">Tere mina olen Header</p>
+</header>
+<wbr>
+<!--k6ikide tagide projekt-->
+<address>
+    Sponsored by <a href="http://www.khk.ee">Tarkade tegude kool</a><br>
+    Tulge ka:<br>
+    Tartu Kutsehariduskeskus<br>
+    Kopli 1, 50115 Tartu
+</address>
+<p><abbr title="Tartu Kutsehariduskeskus">KHK</abbr> visioon: Meistriks kujunemine praktilises õppes</p>
+<img
+    src="https://fbcdn-profile-a.akamaihd.net/hprofile-ak-xfa1/v/t1.0-1/p160x160/10302040_10154153456745525_5058054201514034144_n.png?oh=7a3d6c76dafd59ecabfd9d74a95dcbd9&oe=56755E6F&__gda__=1453720415_696aacd929514b6550481de6d7b5377e"
+    alt="KHK" usemap="#KHKpilt">
+<map name="KHKpilt">
+    <area shape="circle" coords="81, 58, 10" alt="KHK" href="http://www.khk.ee">
+</map>
+<p id="vajuta">Vajutakollasele</p>
+<article>
+    <h1 id="h11"> Esimene</h1>
+
+    <h2 id="h12">
+        <mark>Teine</mark>
+    </h2>
+    <h3 id="h13">Kolmas</h3>
+    <h4 id="h14">Neljas</h4>
+    <h5 id="h15">Viies</h5>
+    <h6 id="h16">Kuues</h6>
+</article>
+<h1 id="h1"> Esimene</h1>
+
+<h2 id="h2"> Esimene</h2>
+
+<h3 id="h3"> Esimene</h3>
+<h4 id="h4"> Esimene</h4>
+<h5 id="h5"> Esimene</h5>
+<h6 id="h6"> Esimene</h6>
+<br>
+    <textarea rows="2" cols="60">
+              Mida iganes siia kirjutada saab
+    </textarea>
+<aside>
+    <h3>Tegelikult</h3>
+
+    <p><bdo dir="rtl">on Esmaspäev hea päev</bdo></p>
+</aside>
+
+<audio controls>
+    <source src="assets/Cow_Moo-Mike_Koenig-42670858.ogg" type="audio/ogg">
+    <source src="assets/Cow_Moo-Mike_Koenig-42670858.mp3" type="audio/mpeg">
+</audio>
+<br>
+<h4>Leadership</h4>
+<blockquote id="tsitaat" cite="http://www.brainyquote.com/quotes/quotes/a/alexandert391181.html?src=t_leadership">
+    I am not afraid of an army of lions led by a sheep; I am afraid of an army of sheep led by a lion.
+</blockquote>
+<button type="button" onclick="alert('Ära näpi seda nuppu!')">I dare you!</button>
+<hr>
+<div id="table">
+    <table border="1">
+        <colgroup>
+            <col span="1" style="background-color:gold">
+            <col style="background-color:darkred">
+        </colgroup>
+        <caption>Hommik</caption>
+        <thead>
+        <tr>
+            <th>Hea</th>
+            <th>Halb</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>Jah</td>
+            <td>Ei</td>
+        </tr>
+        <tr>
+            <td>Miks?</td>
+            <td>Mitte</td>
+        </tr>
+        </tbody>
+        <tfoot>
+        <tr>
+            <td>Ah</td>
+            <td>Ei</td>
+        </tr>
+        </tfoot>
+    </table>
+    <br>
+</div>
+<canvas id="Midagiuut" width="150" height="150" style="border:1px solid darkred;"></canvas>
+<br>
+
+<div class="info">
+    <code>You shall not pass</code><br>
+    <em>You really think so?</em><br>
+    <samp><u>But why?</samp><br>
+    <small>You can't read me</u></small>
+    <br>
+    <strong>I'm your big brother.</strong><br>
+    <var>Hello sister</var><br>
+    <kbd>Keyboard arrived</kbd>
+</div>
+<ul>
+    <li>Make me speak</li>
+</ul>
+<ol>
+    <li>No I can't</li>
+</ol>
+<form action="demo_form.asp" method="get">
+    <input list="Search" name="Search">
+    <datalist id="Search">
+        <option value="Postimees">
+        <option value="Õhtuleht">
+        <option value="Päevaleht">
+        <option value="Elu24">
+        <option value="Maaleht">
+        <option value="Delfi">
+    </datalist>
+    <input type="submit">
+</form>
+<dialog id="dialog" open>OLEN LAHE MEES.</dialog>
+<br>
+<br>
+
+<div id="uus_dialog">
+    <dialog id="dialog" open>Olen tulevikus kasulik kas hüpikakna või muu kasulikuna, et teistega suhelda. Jään teiste
+        peale.
+    </dialog>
+</div>
+<br>
+<dl>
+    <dt>Enter here</dt>
+    <dd>No, this is Space</dd>
+</dl>
+<p>I am
+    <del>dead</del>
+    <ins>ALIVE</ins>
+</p>
+<details>
+    <summary>Alvar makes <dfn>changes</dfn></summary>
+    <p>tagid on <i>lahedad</i></p>
+</details>
+<embed src="whyme.swf"></embed>
+<form>
+    <fieldset>
+        <legend>Informatsioon</legend>
+        Eesnimi: <input type="text"><br>
+        Perenimi: <input type="text"><br>
+        Koduleht: <input type="text">
+    </fieldset>
+</form>
+<figure>
+    <img src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcR3ustJqA8fmPsD-hbAzJ6ZW-xVq0r8vi_-5cNoc1BhRkReM6bF">
+    <figcaption>Pilt tehtud - Nikon D3000</figcaption>
+</figure>
+<iframe src="http://www.khk.ee"></iframe>
+<form action="demo_keygen.asp" method="get">
+    Kasutajanimi: <input type="text" name="usr_name">
+    Whatisthis:
+    <keygen name="kindlustus">
+        <input type="submit">
+</form>
+<main>
+    <h1>Vasta</h1>
+</main>
+<form action="demo_form.asp">
+    <lable for="Jah">Jah</lable>
+    <input type="radio" name="kysimus" id="Jah" value="Jah"><br>
+    <lable for="Ei">Ei</lable>
+    <input type="radio" name="kysimus" id="Ei" value="Ei"><br>
+    <input type="submit" value="Submit">
+</form>
+<meter value="2" min="1" max="10">3 out of 10</meter>
+<nav>
+    <a href="http://www.khk.ee">KHK</a>
+</nav>
+<script>
+    document.write("Siia tuleb mingi tekst")
+</script>
+<noscript>Siia tuli mingi muu tekst</noscript>
+<object width="200" height="200" data="assets/LOGO_3D2.gif"></object>
+<br>
+<select>
+    <optgroup label="Puuviljad">
+        <option value="Pirn">Pirn</option>
+        <option value="Ploom">Ploom</option>
+    </optgroup>
+    <optgroup label="Marjad">
+        <option value="Maasikas">Maasikas</option>
+        <option value="Vaarikas">Vaarikas</option>
+    </optgroup>
+</select><br>
+
+<form id="arvutus" oninput="x.value=parseInt(a.value)+parseInt(b.value)">0
+    <input type="range" id="a" value="50">100
+    +<input type="number" id="b" value="50">
+    =
+    <output name="x" for="a b"></output>
+</form>
+<br>
+    <pre>
+        See on hea asi, free
+        linebreak
+    </pre>
+<progress value="90" max="100">
+</progress>
+<section>
+    <p>Mitu <sub>korda</sub> mahub: <q><sup>3 elevanti</sup></q> ühte autosse</p>
+
+    <p><s>Kuidas sa lendad</s></p>
+
+    <p>Ei lendagi</p>
+</section>
+
+<video width="300" controls>
+    <source src="assets/12sec%20school%20bus%20drag%20race.mp4" type="video/mp4"
+</video>
+<br>
+<footer>
+    Tere
+</footer>
+</body>
+</html>
